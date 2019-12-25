6d2848040539d7fbf4379a78a330255ab2911160
sarzicet17 <tera@tera-X240.localdomain>
2019-12-25 11:14:48 +0900

    構造体の記述

--- my_program/era/com_isreg_func.h
+++ my_program/era/com_isreg_func.h
@@ -3,7 +3,31 @@
 
 typedef struct _ComISRegHandleData{
     GtkWidget *comRegWindow;
-    GtkTreeView *ISinfo;
+
+    //企業インターンシップ登録画面
+    GtkComboBox *yearCombo;
+    GtkComboBox *monthCombo;
+    GtkComboBox *dayCombo;
+    GtkComboBox *daycountCombo;
+    GtkLabel *yearLabel;
+    GtkLabel *monthLabel;
+    GtkLabel *dayLabel;
+    GtkLabel *daycountLabel;
+    GtkButton *isRegistButton;
+    GtkButton *isRegCancelButton;
+    //自社インターン情報取得画面
+    GtkTreeView IsfindList;
+
 }ComISRegHandleData;
+
+typedef struct _ComISRegInfo{
+    gint resRow;
+    gint Year;
+    gint isDays;
+    gchararray isId;
+    gchararray comId;
+    gchararray isTheme;
+    gchararray isDate;
+}ComISRegInfo;
 
 #endif
