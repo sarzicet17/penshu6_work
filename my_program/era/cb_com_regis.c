d331b9ff5a8a1079c960713aa130398f5035e155
sarzicet17 <hm17010@student.miyazaki-u.ac.jp>
2020-01-06 17:05:58 +0900

    企業IS登録の追記

--- my_program/era/cb_com_regis.c
+++ my_program/era/cb_com_regis.c
@@ -1,26 +1,57 @@
 #include "intern_client.h"
 #include "com_isreg_func.h"
 
+
+int isRegWindowShowFlag = 0; //インターン登録画面表示フラグ
+
 G_MODULE_EXPORT void cb_show_com_isreg(GtkMenuItem *menuItem,gpointer data){
     GtkBuilder *builder;
     MainHandleData *hData;
-    
-    ComISRegHandleData *isreghData;
-    ComISRegInfo isregInfo;
 
+    GtkTreeSelection *isreg_selection; //Selectionオブジェクト(自社インターン情報取得画面用)
+    GtkTreeIter isreg_iter;
+    GtkTreeModel *isreg_model;
+    gboolean isreg_success;
+    gboolean isfind_success;
+
+    ComISRegHandleData *isreghData; //ウィジェット格納構造体
+    ComISRegInfo isregInfo; //ツリービューへの格納構造体
+
+    char buffer[BUFSIZE];
 
     hData = (MainHandleData *)data;
     isreghData = (ComISRegHandleData *)malloc(sizeof(ComISRegHandleData));
 
+    //gladeファイルの読み込み
     builder = gtk_builder_new();
     gtk_builder_add_from_file(builder,"com_isreg.glade",NULL);
 
-
+    
     isreghData->comRegWindow = GTK_WIDGET(gtk_builder_get_object(builder,"com_regis_window"));
     isreghData->IsfindList = GTK_TREE_VIEW(gtk_builder_get_object(builder,"isinfo_res_list"));
     hData->isreghData = isreghData;
 
+
+    //インターンシップ情報登録画面の機能
+
     
+
+
+    //インターンシップ情報登録画面機能（ここまで）
+
+
+    //自社インターンシップ情報取得画面の機能
+    isreg_selection = gtk_tree_view_get_selection(hData->treeView);
+    if(!selection) return;
+    
+    model = GTK_TREE_MODEL(gtk_tree_view_get_model(hData->treeView));
+    isfind_success = gtk_tree_selection_get_selected(selection,NULL,isreg_iter);
+
+    if(isfind_succcess){
+        gtk_tree_model_get()
+    }
+
+    //自社インターンシップ情報取得画面の機能(ここまで)
 
 
     gtk_builder_connect_signals(builder,hData);
--- my_program/era/com_isreg_func.h
+++ my_program/era/com_isreg_func.h
@@ -1,6 +1,8 @@
 #ifndef _COM_ISREG_HDR_
 #define _COM_ISREG_HDR_
 
+#include <gtk/gtk.h>
+
 typedef struct _ComISRegHandleData{
     GtkWidget *comRegWindow;
 
@@ -15,8 +17,10 @@ typedef struct _ComISRegHandleData{
     GtkLabel *daycountLabel;
     GtkButton *isRegistButton;
     GtkButton *isRegCancelButton;
+    GtkTreeSelection *selection;
     //自社インターン情報取得画面
-    GtkTreeView *IsfindList;
+    GtkButton *isListSearchButton;
+    GtkTreeView *isInfoResList;
 
 }ComISRegHandleData;
 
